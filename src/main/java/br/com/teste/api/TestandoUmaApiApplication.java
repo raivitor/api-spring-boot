00497ac61b56466cd239c5e907d6fe650fe93df2
silva.ext rai <rai.silva.ext@sodexo.com>
2022-02-17 17:34:33 -0300

    Initial commit

---
+++ src/main/java/br/com/teste/api/TestandoUmaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoUmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoUmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/api/TestandoUmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestandoUmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
